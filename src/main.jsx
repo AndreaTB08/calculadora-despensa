06de926cc85ae3e2ae0eb703e8c22a15f143377e
AndreaTB08 <andreat.atb@gmail.com>
2025-11-11 21:06:03 -0600

    Corregir main.jsx después de conflicto

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppPrincipal from './AppPrincipal.jsx'
@@ -9,12 +8,3 @@ createRoot(document.getElementById('root')).render(
     <AppPrincipal />
   </React.StrictMode>
 )
-=======
-// Punto de entrada principal
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppPrincipal from './AppPrincipal';
-import './estilos.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<AppPrincipal />);
->>>>>>> 386adf5bbb077c919e237888cf79df7738f54bc6
